13d70339fb90b6e9fe9cb691fd948024af2699b6
FISSO\ignaz <ignazio.abbate95@gmail.com>
2025-12-07 15:23:57 +0100

    disabled bltouch

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1457,7 +1457,7 @@
  * The probe replaces the Z-MIN endstop and is used for Z homing.
  * (Automatically enables USE_PROBE_FOR_Z_HOMING.)
  */
-#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+// #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
 // #define USE_PROBE_FOR_Z_HOMING
@@ -1517,7 +1517,7 @@
 /**
  * The BLTouch probe uses a Hall effect sensor and emulates a servo.
  */
-#define BLTOUCH
+// #define BLTOUCH
 
 /**
  * MagLev V4 probe by MDD
